c221394d9f8ee7544aa49d90a64973b6a7691ce2
Jesse Panganiban <me@jpanganiban.com>
2012-06-24 23:24:41 +0800

    Added backbone base classes
    Removed indentation to save space

---
+++ spec/spec/classes.js
@@ -0,0 +1,27 @@
+describe("Base Classes", function() {
+
+  describe("BaseView", function() {
+
+    it("should exist", function() {
+      expect(Uptimr.BaseView).toBeDefined();
+    });
+
+  });
+
+  describe("BaseModel", function() {
+
+    it("should exist", function() {
+      expect(Uptimr.BaseModel).toBeDefined();
+    });
+
+  });
+
+  describe("BaseCollection", function() {
+
+    it("should exist", function() {
+      expect(Uptimr.BaseCollection).toBeDefined();
+    });
+
+  });
+
+});
--- src/static/js/uptimr.js
+++ src/static/js/uptimr.js
@@ -1,3 +1,14 @@
 var Uptimr = { views: {}, models: {}, collections: {} };
 var Parse = { appId: 'PXsT5VE2Qaizs3yDnR8QtfQdSRp0vxQTI9eUOa15'
               , restKey: 'oKwodD8WETS50UHf16TLTNAcLHfJR6bh9fgiFjxh' };
+
+
+(function($, undefined) {
+
+Uptimr.BaseView = Backbone.View.extend();
+
+Uptimr.BaseModel = Backbone.Model.extend();
+
+Uptimr.BaseCollection = Backbone.Collection.extend();
+
+})(jQuery);
