e1bfa45f3d7588329f0ecfebac1d2a6738324b10
Jesse Panganiban <me@jpanganiban.com>
2012-06-24 23:15:54 +0800

    Added views, models, collections in Application namespace.

--- spec/spec/main.js
+++ spec/spec/main.js
@@ -1,7 +1,23 @@
 describe("Main Application", function() {
 
-  it("should have the Uptimr object", function() {
-    expect(Uptimr).toBeDefined();
+  describe("Uptimr namespace", function() {
+
+    it("should exist", function() {
+      expect(Uptimr).toBeDefined();
+    });
+
+    it("should have 'views' object", function() {
+      expect(Uptimr.views).toBeDefined();
+    });
+
+    it("should have 'models' object", function() {
+      expect(Uptimr.models).toBeDefined();
+    });
+
+    it("should have 'collections' object", function() {
+      expect(Uptimr.collections).toBeDefined();
+    });
+
   });
 
 });
--- src/static/js/uptimr.js
+++ src/static/js/uptimr.js
@@ -1,1 +1,3 @@
 var Uptimr = { views: {}, models: {}, collections: {} };
+var Parse = { appId: 'PXsT5VE2Qaizs3yDnR8QtfQdSRp0vxQTI9eUOa15'
+              , restKey: 'oKwodD8WETS50UHf16TLTNAcLHfJR6bh9fgiFjxh' };
